566beeb26b71de1ebd9e19e39cc8e7580dee4fe3
XenoAmess <xenoamess@gmail.com>
2020-06-03 22:16:25 +0800

    reduce

--- src/test/java/org/apache/commons/lang3/performance/StandardCharsetPerformanceTest.java
+++ src/test/java/org/apache/commons/lang3/performance/StandardCharsetPerformanceTest.java
@@ -18,10 +18,12 @@ package org.apache.commons.lang3.performance;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
@@ -33,6 +35,8 @@ import java.util.concurrent.TimeUnit;
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Thread)
+@Warmup(iterations = 5, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 5, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
 public class StandardCharsetPerformanceTest {
     private static final String TEST_STRING_LONG = "Stack Overflow\n" +
             "Products\n" +
